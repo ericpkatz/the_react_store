948e524f9f7cbc5d64f98196b6e0207bd1ce437f
ericpkatz <ericpkatz@gmail.com>
2023-03-08 19:56:22 -0500

    show products

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const Product = conn.define('product', {
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
 
 app.get('/api/products', async(req, res, next)=> {
   try {
